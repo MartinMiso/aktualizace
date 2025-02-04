bfb2f944e0a6b8cd8fa1f41ed381383ec987ad8f
MartinMiso <martin.miso@seznam.cz>
2025-02-04 20:25:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -209,6 +209,7 @@ updater = update.Update(RAW_GITHUB_URL)
 
 # Kontrola a aktualizace souboru main.py
 updater.compare_and_update("main.py")
+
 # načtení 'prázdné' váhy
 first_weight = load_first_weight()
 
@@ -229,24 +230,26 @@ while True:
         # měření síly wifi
         rssi = get_wifi_signal_strength()
         # Měření frekvence
-        #frekvence = measure_freq()
+        
 
         prumer = []  # Definice seznamu pro průměr
 
         try:
-            for _ in range(5):  # Cyklus poběží 5krát
-                time.sleep(0.5)
+            for _ in range(10):  # Cyklus poběží 10krát
+                time.sleep(1)
                 frekvence = measure_freq()  # Měření frekvence
                 prumer.append(frekvence)  # Uložení do seznamu
 
-                prum_frekvence = sum(prumer) / len(prumer)  # Výpočet průměru
+            prum_frekvence = sum(prumer) / len(prumer)  # Výpočet průměru
+            print(f"Průměrná frekvence: {prum_frekvence} Hz")
 
             if 95 < prum_frekvence < 260:  # Kontrola frekvence
                 send_whatsapp("420733113537", "3142801")  # Pošle upozornění
                 #send_whatsapp("420603498872", "4097369")  # Jirka
                 print("Asi se rojíme")
     
-            print("Vše OK")  # Pokud cyklus doběhne bez výstrahy
+            else:
+                print("Vše OK")  # Pokud cyklus doběhne bez výstrahy
         except Exception as e:
             print("Chyba poslání notifikace:", e)
 
@@ -254,6 +257,7 @@ while True:
         send_data(temp_aht, hum_aht, temp_bmp, pres_bmp, weight, rssi, prum_frekvence)
     except Exception as e:
         print("Chyba senzoru:", e)
-      
-    #deep_sleep(600000) # Hluboký spánek na 10 minut
+     
+    #time.sleep(20)
+    deep_sleep(600000) # Hluboký spánek na 10 minut
 
