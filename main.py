3cc93ea02b5a5f1971cf37927b3642f24f5471f9
MartinMiso <martin.miso@seznam.cz>
2025-02-04 20:09:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -249,6 +249,11 @@ while True:
         print("Vše OK")  # Pokud cyklus doběhne bez výstrahy
     except Exception as e:
         print("Chyba poslání notifikace:", e)
-             
+
+    # odeslání dat
+        send_data(temp_aht, hum_aht, temp_bmp, pres_bmp, weight, rssi, frekvence)
+    except Exception as e:
+        print("Chyba senzoru:", e)
       
     deep_sleep(600000) # Hluboký spánek na 10 minut
+
